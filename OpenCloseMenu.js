190b2207d6e2943f3728c094d348ac9b0ef9091f
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2017-03-04 03:15:31 +0000

    Create OpenCloseMenu.js

---
+++ OpenCloseMenu.js
@@ -0,0 +1,41 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav1() {
+    document.getElementById("at_inner").style.width = "250px";
+    
+}
+
+function openNav2() {
+    document.getElementById("mc_inner").style.width = "250px";
+    
+}
+
+function openNav3() {
+    document.getElementById("proto_inner").style.width = "250px";
+    
+}
+
+function openNav4() {
+    document.getElementById("au_inner").style.width = "250px";
+        
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav1() {
+    document.getElementById("at_inner").style.width = "0";
+  
+}
+
+function closeNav2() {
+    document.getElementById("mc_inner").style.width = "0";
+  
+}
+
+function closeNav3() {
+    document.getElementById("proto_inner").style.width = "0";
+  
+}
+
+function closeNav4() {
+    document.getElementById("au_inner").style.width = "0";
+  
+}
