7a562e1d7198148403382a2c0c2ad5c1e6335159
Anfifo <afonseca-1995@hotmail.com>
2017-03-08 16:55:24 +0000

    beautification

--- OpenCloseMenu.js
+++ OpenCloseMenu.js
@@ -1,41 +1,14 @@
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav1() {
-    document.getElementById("at_inner").style.width = "250px";
-    
-}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("Menu").style.visibility="hidden";
 
-function openNav2() {
-    document.getElementById("mc_inner").style.width = "250px";
-    
-}
-
-function openNav3() {
-    document.getElementById("proto_inner").style.width = "250px";
-    
-}
-
-function openNav4() {
-    document.getElementById("au_inner").style.width = "250px";
-        
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav1() {
-    document.getElementById("at_inner").style.width = "0";
-  
-}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("Menu").style.visibility="visible";
 
-function closeNav2() {
-    document.getElementById("mc_inner").style.width = "0";
-  
-}
-
-function closeNav3() {
-    document.getElementById("proto_inner").style.width = "0";
-  
-}
-
-function closeNav4() {
-    document.getElementById("au_inner").style.width = "0";
-  
 }
